b1b92ba6f1f7ec66482954fee0bcda46d1b64813
userVII <userVII@users.noreply.github.com>
2025-12-08 22:42:30 -0500

    one final push

--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
